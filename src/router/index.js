2b9e7b756ce7599fe7151735d9f4857c2a9d81f4
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-04-18 21:17:03 +0700

    add Board component with default data

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Board from '../views/Board.vue'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'board',
+      component: Board
+    }
+  ]
 })
 
 export default router
---
+++ src/views/Board.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="board">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-gray-900 no-underline;
+}
+
+.column {
+  @apply bg-gray-300 p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
+.board {
+  @apply p-4 bg-teal-600 h-full overflow-auto;
+}
+
+.task-bg {
+  @apply inset-0 absolute;
+  background: rgba(0,0,0,0.5);
+}
+</style>
